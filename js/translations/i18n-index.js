ca8af310bb3dbd634ccbca0463f422e32ddbb214
adavice <97439259+adavice@users.noreply.github.com>
2026-07-23 12:42:36 +0300

    Update i18n-index.js

--- js/translations/i18n-index.js
+++ js/translations/i18n-index.js
@@ -345,3 +345,5 @@ export const translations = {
     "All rights reserved.": "Tous droits réservés",
     "All brand names, (registered) trademarks, images and product descriptions are the property of their legal owners and are neither sponsor nor partner of this promotion.": "Toutes les marques, marques déposées, images et descriptions de produits sont la propriété de leurs détenteurs légaux respectifs et ne sont ni sponsors ni partenaires de cette promotion."
   ,
+  }
+}
